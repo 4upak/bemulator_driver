a326555a292ca63d02a75963a91f9420f693ca5c
4upak <serg.chupak@gmail.com>
2023-04-06 22:09:08 +0300

    bid add function

--- functions.py
+++ functions.py
@@ -4,12 +4,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+def get_mfa_code():
+    return input('Enter MFA code: ')
+
 def login(driver):
-    driver.get('https://binance.com')
-
-    #wait = WebDriverWait(driver, 1000000)
-    #wait.until(EC.presence_of_element_located((By.ID, ['header_login','header_menu_cabinet','mobile_header_menu_cabinet'])))
-
+    driver.get('https:///p2p.binance.com')
     while True:
         try:
             if driver.find_element(By.ID, 'header_deposit_drawer'):
@@ -27,10 +26,20 @@ def login(driver):
             time.sleep(1)
             continue
 
+        try:
+            print('Checking for coockies button')
+            accept_coockies_button = driver.find_element(By.XPATH, "//button[contains(text(),'I accept')]")
+            if accept_coockies_button:
+                accept_coockies_button.click()
+                continue
+        except Exception as e:
+            pass
+
     while True:
         try:
             cabinet = driver.find_element(By.ID, 'header_deposit_drawer')
             if cabinet:
+                driver.get('https:///p2p.binance.com')
                 print('Login success')
                 return driver
         except Exception as e:
@@ -39,15 +48,174 @@ def login(driver):
             continue
 
 
-
 def quit(webdriver):
     pass
 
-def add_payment_method(webdriver):
-    pass
+def add_payment_method(driver,card_number,card_holder_name,bank_name):
+    #delete old payment methods
+    driver.get('https://p2p.binance.com/en/userCenter#payment')
 
-def add_bid(webdriver):
-    pass
+
+    wait = WebDriverWait(driver, 20)
+    wait.until(EC.presence_of_element_located((By.ID, "tab-payment")))
+    print("Payment methods page loaded")
+
+    time.sleep(5)
+
+    buttons = driver.find_elements(By.XPATH,"//button[contains(text(),'Delete')]")
+
+    #find all buttons with text Delete
+
+
+
+    #find elements by xpath using Bi
+    print(f"{len(buttons)} payment methods found")
+
+
+    for button in buttons:
+        button.click()
+        time.sleep(2)
+        driver.find_element(By.XPATH,"//div[contains(text(),'Confirm')]").click()
+        time.sleep(2)
+
+    print("All payment methods deleted")
+
+    url = f"https://p2p.binance.com/en/payment/add/{bank_name}"
+    driver.get(url)
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'PaymentMethodForm__form_wrapper')))
+
+    input_card_number = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Please enter your bank account number"]')
+    input_card_number.send_keys(card_number)
+
+    input_bank_name = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Enter the name of your bank"]')
+    input_bank_name.send_keys(card_holder_name)
+
+    confirm_button = driver.find_element(By.XPATH,"//button[contains(text(),'Confirm')]")
+    confirm_button.click()
+
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.ID, 'mfa-google-input')))
+
+    mfa_input = driver.find_element(By.ID, 'mfa-google-input')
+    mfa_code = get_mfa_code()
+    mfa_input.send_keys(mfa_code)
+
+    print("MFA code entered")
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'PaymentMethodForm__form_wrapper')))
+    print("Payment method added")
+
+    return driver
+
+def add_bid(driver, currency, amount, min_amount, autoreplay_text):
+    url = 'https://p2p.binance.com/en/myads?type=normal&code=default'
+    driver.get(url)
+
+    wait = WebDriverWait(webdriver, 10)
+    wait.until(EC.presence_of_element_located((By.ID, 'C2C_p2pMyAdsList_filter_btn_filter')))
+    time.sleep(4)
+
+    close_element = driver.find_element(By.ID, "C2C_p2pMyAdsList_management_btn_close")
+    if close_element:
+        print("Other bid found")
+        close_element.click()
+        print("Close element clicked")
+
+        wait = WebDriverWait(webdriver, 10)
+        wait.until(EC.presence_of_element_located((By.XPATH,"//div[contains(text(),'Close')]")))
+
+        driver.find_element(By.XPATH,"//div[contains(text(),'Close')]").click()
+        print("Close button clicked")
+
+    wait = WebDriverWait(webdriver, 10)
+    wait.until(EC.presence_of_element_located((By.ID, 'C2C_p2pNav_btn_postNewAd')))
+
+    EC.presence_of_element_located((By.ID, 'C2C_p2pNav_btn_postNewAd')).click()
+    print("New bid button clicked")
+
+    wait = WebDriverWait(webdriver, 10)
+    wait.until(EC.presence_of_element_located((By.XPATH,"//div[contains(text(),'I want to sell')]")))
+
+    driver.find_element(By.XPATH,"//div[contains(text(),'I want to sell')]").click()
+
+    wait = WebDriverWait(webdriver, 10)
+    wait.until(EC.presence_of_element_located((By.ID, "C2C_p2pPost_step1_price_input")))
+
+    print("Currency input found")
+    input = driver.find_element(By.ID, "C2C_p2pPost_step1_price_input")
+    input.send_keys(currency)
+    print("Currency entered")
+
+    driver.find_element(By.ID,"C2C_p2pPost_step1_btn_next").click()
+    print("Next button clicked")
+
+    #find input with name="initAmount"
+    wait = WebDriverWait(webdriver, 10)
+    wait.until(EC.presence_of_element_located((By.NAME, "initAmount")))
+    print("Amount input found")
+
+    input = driver.find_element(By.NAME, "initAmount")
+    input.send_keys(amount)
+    print("Amount entered")
+
+    input = driver.find_element(By.NAME, "minOrderPrice")
+    input.send_keys(min_amount)
+    print("Min amount entered")
+
+    #fin button with css selector 'from button[data-bn-type="button"]'
+    driver.find_element(By.CSS_SELECTOR, 'button[data-bn-type="button"]').click()
+
+    wait = WebDriverWait(webdriver, 10)
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME,"SellPaymentForm__StyledCard-c6pg06-0")))
+
+    driver.find_element(By.CLASS_NAME,"SellPaymentForm__StyledCard-c6pg06-0").click()
+    time.sleep(1)
+
+    wait = WebDriverWait(webdriver, 10)
+    wait.until(EC.presence_of_element_located((By.ID, "C2C_p2pPost_step2_btn_next")))
+
+    driver.find_element(By.ID, "C2C_p2pPost_step2_btn_next").click()
+    print("Next button clicked")
+
+    #find textarea with name="autoReplyMsg"
+    wait = WebDriverWait(webdriver, 10)
+    wait.until(EC.presence_of_element_located((By.NAME, "autoReplyMsg")))
+    print("Auto reply input found")
+
+    textarea = driver.find_element(By.NAME, "autoReplyMsg")
+    textarea.send_keys(autoreplay_text)
+    time.sleep(1)
+
+    # click on button with id "C2C_p2pPost_step3_btn_publish"
+    driver.find_element(By.ID, "C2C_p2pPost_step3_btn_publish").click()
+    time.sleep(1)
+
+    #click on class with text "Confirm to Post"
+    driver.find_element(By.XPATH,"//div[contains(text(),'Confirm to Post')]").click()
+    print("Confirm to post clicked")
+
+    wait = WebDriverWait(webdriver, 10)
+    wait.until(EC.presence_of_element_located((By.ID,"C2C_p2pMyAdsList_management_btn_edit")))
+    print("Bid added")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def get_order_list(webdriver):
     pass
--- main.py
+++ main.py
@@ -3,28 +3,30 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from selenium import webdriver
-
+import pickle
+import dill
 import time
 from functions import *
 import websocket
 import logging
 import json
+from pathlib import Path
+from seleniumwire import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 
 
-account = "serg_chupak@gmail.com"
-driver = webdriver.Chrome('chromedriver')
-engage = False
+account = "serg.chupak@gmail.com"
+driver = webdriver.Chrome()
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-
 def on_message(ws, message):
     global driver
     global account
-    print(message)
 
     try:
         message = json.loads(message)
@@ -35,16 +37,38 @@ def on_message(ws, message):
 
             if action == 'login':
                 print(action)
-                print(message_dict['data'])
                 data = message_dict['data']
+                print(data['account'])
                 action_account = data['account']
+
                 if str(action_account) == str(account):
                     driver = login(driver)
                 else:
                     print(action_account + " != " + account)
 
+
+            if action == 'add_payment_method':
+
+                print(action)
+                data = message_dict['data']
+                print(data['account'])
+                action_account = data['account']
+                if str(action_account) == str(account):
+                    driver = add_payment_method(driver, data['card_number'], data['name_on_card'], data['bank_name'])
+                else:
+                    print(action_account + " != " + account)
+
     except Exception as e:
         print(e)
+
+    '''try:
+        Path('cookies.json').write_text(
+            json.dumps(driver.get_cookies(), indent=2)
+        )
+        print("cookies saved")
+    except Exception as e:
+        print(e)'''
+
 
 
 
@@ -53,34 +77,42 @@ def on_error(ws, error):
 
 def on_close(ws):
     print("WebSocket closed")
-    driver.quit()
+    #reconnect websocket
+
+
 
 def on_open(ws):
     print("WebSocket opened")
+
     # send a message when the WebSocket is opened
     json_message = {"type": "emulator_message", "message": "Hello, world!"}
     ws.send(json.dumps(json_message))
 
-    def send_ping():
-        while ws.keep_running:
-            ws.send_ping()
-            time.sleep(5)
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+
+
+
     logging.basicConfig()
     logger = logging.getLogger('websocket')
     logger.setLevel(logging.DEBUG)
-    ws = websocket.WebSocketApp("ws://0:8000/ws/emulator/emulator/",
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close,
-                                on_open=on_open,
+    while True:
+        try:
+            ws = websocket.WebSocketApp("ws://0:8000/ws/emulator/emulator/",
+                                        on_message=on_message,
+                                        on_error=on_error,
+                                        on_close=on_close,
+                                        on_open=on_open,
 
-                                )
-    ws.on_open = on_open
-    ws.run_forever(ping_timeout=120)
+                                        )
+            ws.on_open = on_open
+            ws.run_forever(ping_timeout=120)
+        except Exception as e:
+            print(f"WebSocket error: {e}")
+            time.sleep(1)
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
